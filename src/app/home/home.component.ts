939917be50da1fae642ff80984c556d5114eca16
Toan Phung <toan.phung@proteo.info>
2024-06-17 22:17:26 +0100

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {BannerComponent} from "../banner/banner.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    BannerComponent
+    BannerComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
