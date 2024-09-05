c86da357541ab8c2c6b9f189c40f384d2f84786c
Toan Phung <toan.phung@proteo.info>
2024-09-05 17:26:00 +0100

    update

--- src/app/collate/collate-search/collate-search.component.ts
+++ src/app/collate/collate-search/collate-search.component.ts
@@ -8,6 +8,9 @@ import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {MatCard, MatCardContent} from "@angular/material/card";
 import {WebsocketService} from "../../websocket.service";
+import {MatOption} from "@angular/material/core";
+import {MatSelect} from "@angular/material/select";
+import {MatLabel} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-collate-search',
@@ -20,7 +23,10 @@ import {WebsocketService} from "../../websocket.service";
     MatIconButton,
     MatIcon,
     MatCardContent,
-    MatCard
+    MatCard,
+    MatOption,
+    MatSelect,
+    MatLabel
   ],
   templateUrl: './collate-search.component.html',
   styleUrl: './collate-search.component.scss'
@@ -29,7 +35,8 @@ export class CollateSearchComponent {
   @Input() projects: Project[] = [];
 
   form = this.fb.group({
-    searchQuery: new FormControl<string>('', Validators.required)
+    searchQuery: new FormControl<string>('', Validators.required),
+    searchMode: new FormControl<"pi"|"gene"|"uniprot">("gene", Validators.required),
   });
   @Output() searchResultID: EventEmitter<number> = new EventEmitter<number>();
 
@@ -65,13 +72,14 @@ export class CollateSearchComponent {
       }
     }
 
-    if (this.form.value.searchQuery) {
+    if (this.form.value.searchQuery && this.form.value.searchMode) {
       this.web.createSearch(
         analysisGroupIDs,
         this.form.value.searchQuery,
         this.web.searchSessionID,
         0.0001,
-        0.0001
+        0.0001,
+        this.form.value.searchMode
       ).subscribe(() => {
         this.sb.open('Search queued', 'Dismiss', { duration: 2000 });
       });
