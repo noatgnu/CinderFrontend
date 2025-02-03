6e5f6bc8582c2d37b287680e9d6266dc1193f815
Toan Phung <toan.phung@proteo.info>
2025-02-03 20:02:04 +0000

    Update cytoscape-plot.component.ts

--- src/app/collate/cytoscape-plot/cytoscape-plot.component.ts
+++ src/app/collate/cytoscape-plot/cytoscape-plot.component.ts
@@ -22,11 +22,11 @@ function popperFactory(ref: any, content: any, opts: any) {
   console.log(content)
   console.log(opts)
   function update() {
-    console.log(ref.getBoundingClientRect())
+    const bounded = ref.getBoundingClientRect()
     computePosition(ref, content, popperOptions).then(({x, y}) => {
       console.log(x, y)
       const result = Object.assign(content.style, {
-        left: `${x}px`,
+        left: `${bounded.left}px`,
         top: `${y}px`,
         position: 'absolute',
         transform: 'translate(-50%, -50%)'
