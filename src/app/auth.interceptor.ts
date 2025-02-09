0948fd95942be4079640652ec63a69c0732d66ca
Toan Phung <toan.phung@proteo.info>
2025-02-09 15:49:42 +0000

    Update auth.interceptor.ts

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -6,33 +6,36 @@ import {WebService} from "./web.service";
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const accountService = inject(AccountsService)
   const webService = inject(WebService)
-  req = req.clone({
-    setHeaders: {"X-Cinder-Instance-Id": webService.cinderInstanceID}
-  })
-  const csrf = webService.getCSRFTokenFromCookies()
-  console.log(csrf)
-  if (csrf) {
-    // check if the csrf token is in the headers
+  if (req.url.startsWith(webService.baseURL)) {
     req = req.clone({
-      setHeaders: {"X-CSRFToken": csrf}
+      setHeaders: {"X-Cinder-Instance-Id": webService.cinderInstanceID}
     })
+    const csrf = webService.getCSRFTokenFromCookies()
+    console.log(csrf)
+    if (csrf) {
+      // check if the csrf token is in the headers
+      req = req.clone({
+        setHeaders: {"X-CSRFToken": csrf}
+      })
+    }
+    const session = webService.getSessionIDFromCookies()
+    if (session) {
+      req = req.clone({
+        setHeaders: {"X-Session-Token": session}
+      })
+    }
+    if (accountService.loggedIn) {
+      req = req.clone({
+        withCredentials: true
+      })
+    }
+    if (accountService.userAccount.token) {
+      req = req.clone({
+        setHeaders: { Authorization: `Token ${accountService.userAccount.token}` }
+      });
+    }
   }
-  const session = webService.getSessionIDFromCookies()
-  if (session) {
-    req = req.clone({
-      setHeaders: {"X-Session-Token": session}
-    })
-  }
-  if (accountService.loggedIn) {
-    req = req.clone({
-      withCredentials: true
-    })
-  }
-  if (accountService.userAccount.token) {
-    req = req.clone({
-      setHeaders: { Authorization: `Token ${accountService.userAccount.token}` }
-    });
-  }
+
 
   return next(req);
 };
