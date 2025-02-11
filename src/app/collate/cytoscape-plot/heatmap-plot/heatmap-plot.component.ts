4d19b5d5e98f54b1a7c544339d7a310c8a0314d9
Toan Phung <toan.phung@proteo.info>
2025-02-11 14:56:56 +0000

    Update heatmap-plot.component.ts

--- src/app/collate/cytoscape-plot/heatmap-plot/heatmap-plot.component.ts
+++ src/app/collate/cytoscape-plot/heatmap-plot/heatmap-plot.component.ts
@@ -42,15 +42,19 @@ export class HeatmapPlotComponent {
   graphData: any = {}
   revision = 0
   drawHeatmap() {
+    // reorder data based on project
+    this.data = this.data.sort((a, b) => a.project.localeCompare(b.project));
     // transform data to heatmap
     const x = this.data.map(d => `${d.analysis_group} ${d.conditionA} vs ${d.conditionB}`);
     const y = this.data.map(d => d.protein);
     const z = this.data.map(d => d.log2fc);
+    const text = this.data.map(d => `${d.comparison}<br>p-value: ${d.p_value}<br>log2fc: ${d.log2fc}<br>project: ${d.project}<br>analysis_group: ${d.analysis_group}<br>conditionA: ${d.conditionA}<br>conditionB: ${d.conditionB}`);
 
     const trace = {
       x: x,
       y: y,
       z: z,
+      text: text,
       type: 'heatmap',
       colorscale: 'Viridis'
     };
