9c31bde30e6fcb52bf53a0d504c5e17c9392399a
Toan Phung <toan.phung@proteo.info>
2025-03-26 13:32:49 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, appConfig).then(() => {
+  const loadingScreen = document.getElementById('loading-screen');
+  if (loadingScreen) {
+    loadingScreen.style.display = 'none';
+  }
+})
   .catch((err) => console.error(err));
